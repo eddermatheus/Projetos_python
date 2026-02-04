86e3d6b961f3e504f5b87f4eff838cd1619037f4
Edder Matheus <eddermatheus0@gmail.com>
2026-02-03 22:23:18 -0300

    Tabuada

--- Desafios/Tabuada.py
+++ Desafios/Tabuada.py
@@ -3,5 +3,5 @@ tabuada = int(input('Tabuada de que número? : '))
 print(f'A tabuada do {tabuada} abaixo: ')
 for c in range (1,11): #o final é 11 pois é sempre o final n - 1 , sendo n um numero inteiro
   resultado= tabuada * c
-  print(resultado, end = ' ') #imprimir tudo ao lado do outro com espaço
+  print(f'{tabuada} x {c} = {resultado}')
 
