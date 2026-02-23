6752c2db8806dfd9a66898368658e5def855b273
Edder Matheus <eddermatheus0@gmail.com>
2026-02-23 12:03:49 -0300

    comiteicoment

--- Desafios/Palindromo.py
+++ Desafios/Palindromo.py
@@ -2,7 +2,7 @@ nome = input(('Digite um nome: ')).strip()
 separar = nome.split() #separando em palavras
 junto = ''.join(separar)  #juntando as palavras que eu havia separado
 inverso = ''
-for letra in range(len(junto) - 1 , -1, -1): # esse len(junto) é um numero o tamanho total que conta decrescendo até 0
+for letra in range(len(junto) - 1 , -1, -1): # esse len(junto) é um numero o tamanho total que conta decrescendo de 1 em 1 até 0
    inverso+= junto[letra] # aq o inverso vai ser somado com as letras de trás pra frente, ou seja, formar a palavra inversa
 if inverso == junto:  #if pra saber se é palindromo
    print('É um palindromo')
