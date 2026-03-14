be9c1088827905da5436c50e96b1b33167dbbb9f
Edder Matheus <eddermatheus0@gmail.com>
2026-03-14 11:15:14 -0300

    listinha

--- Desafios/Python_Engenharia/Projeto_1.py
+++ Desafios/Python_Engenharia/Projeto_1.py
@@ -1,1 +1,10 @@
-print('Hellow, Word!')
+Siemens = ["S7-1500", "S7-300"]
+AllenBradley = ["MicroLogix", "CompactLogix", "ControlLogix"]
+SchneiderElectric = ["M221", "M241", "M340", "M580"]
+CLPs = [Siemens, AllenBradley, SchneiderElectric]
+
+if "S7-1200" not in CLPs[0]:
+ CLPs[0].insert(1,"S7-1200")
+ print(CLPs[0])
+else:
+ print(False)
