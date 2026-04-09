07f6529f1aa674a7261ad2a54f3c2cc27995bf15
Edder Matheus <eddermatheus0@gmail.com>
2026-04-08 23:04:54 -0300

    escrita

---
+++ Desafios/Python_Engenharia/Escrita_cliente.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+with open("clientes.txt", "w",encoding="utf-8") as arq:
+  for  i in range (3):
+   print(f"Cadastro do cliente {i+1}")
+   nome = input("Nome completo: ")
+   chave_pix = input("Chave pix: ")
+   cpf = input("Digite o CPF: ")
+   arq.write(f"{nome}, {chave_pix},{cpf}\n")
+   
